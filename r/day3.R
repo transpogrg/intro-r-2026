8decb1283a328dadeb97b2002c22ea173d7b5bf4
Vielhaber <VielhabG@trimet.org>
2026-03-20 11:27:51 -0700

    functions

--- r/day3.R
+++ r/day3.R
@@ -65,3 +65,25 @@ as.data.frame()
   
 colnames(starttime_seq) <- c("starttime")
 
+figure_function <- function(stid, measure){
+  sta_1059 <- stations_df  |>
+    filter(stationid == stid) |>
+    right_join(starttime_seq, by = "starttime") |> 
+    ggplot(aes(x = starttime, y = {{measure}})) +
+    geom_line(color = "skyblue") +
+    geom_point(color = "darkblue") +
+    scale_x_datetime(
+      date_breaks = "1 day",
+      date_labels = "%Y-%m-%d",
+      guide = guide_axis(angle = 45)
+    ) +
+    xlab(NULL) +
+    theme_bw() +
+    geom_hline(yintercept = mean(stations_df$tot_volume),
+               color = "pink")
+
+  sta_1059  
+  
+}
+
+figure_function(3142, mean_speed)
